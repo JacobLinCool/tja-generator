66c65ac7399a365520818b73f672cc8bac13d96f
JacobLinCool <jacoblincool@gmail.com>
2024-08-29 09:28:57 +0800

    fix: no note

--- synthesize.py
+++ synthesize.py
@@ -73,6 +73,20 @@ def detect(don_inference, ka_inference, delta=0.05):
     return don_timestamp, ka_timestamp
 
 
+# def note_to_drumroll(timestamp, max_gap=5, min_note=3):
+#     drumroll = []
+#     note = 0
+#     for i in range(1, len(timestamp)):
+#         if timestamp[i] - timestamp[i - 1] <= max_gap:
+#             note += 1
+#         else:
+#             if note >= min_note:
+#                 drumroll.append((timestamp[i - note - 1], timestamp[i - 1]))
+#             note = 0
+#     if note >= min_note:
+#         drumroll.append((timestamp[-note - 1], timestamp[-1]))
+#     return drumroll
+
 def synthesize(don_timestamp, ka_timestamp, song, filepath):
     song.don_timestamp = don_timestamp
     song.timestamp = song.don_timestamp * 512 / song.samplerate
@@ -99,28 +113,36 @@ def create_tja(
     tja = f"TITLE: {title}\nSUBTITLE: {subtitle}\nBPM: 240\nWAVE:{wave}\nOFFSET:0\n\n"
 
     for i, (don, ka) in enumerate(timestamps):
-        level = [3, 5, 7, 8, 9][i]
-        scroll = [0.6, 0.7, 0.8, 0.9, 1.0][i]
-        tja += f"COURSE:{i}\nLEVEL:{level}\n\n#START\n#SCROLL {scroll}\n"
+        try:
+            level = [3, 5, 7, 8, 9][i]
+            scroll = [0.6, 0.7, 0.8, 0.9, 1.0][i]
 
-        don_timestamp = np.rint(don * 512 / song.samplerate * 100).astype(np.int32)
-        ka_timestamp = np.rint(ka * 512 / song.samplerate * 100).astype(np.int32)
-        length = np.max((don_timestamp[-1], ka_timestamp[-1]))
-        safezone_keep = 0
-        for time in range(length):
-            if np.isin(time, don_timestamp) == True and safezone_keep <= 0:
-                tja += "1"
-                safezone_keep = safezone
-            elif np.isin(time, ka_timestamp) == True and safezone_keep <= 0:
-                tja += "2"
-                safezone_keep = safezone
-            else:
-                tja += "0"
-                safezone_keep -= 1
-            if time % 100 == 99:
-                tja += ",\n"
-        if length % 100 != 0:
-            tja += "0" * (100 - (length % 100)) + ",\n"
-        tja += "#END\n\n"
+            don_timestamp = np.rint(don * 512 / song.samplerate * 100).astype(np.int32)
+            ka_timestamp = np.rint(ka * 512 / song.samplerate * 100).astype(np.int32)
+            length = np.max(
+                (
+                    don_timestamp[-1] if don_timestamp.size > 0 else 0,
+                    ka_timestamp[-1] if ka_timestamp.size > 0 else 0,
+                )
+            )
+            safezone_keep = 0
+            tja += f"COURSE:{i}\nLEVEL:{level}\n\n#START\n#SCROLL {scroll}\n"
+            for time in range(length):
+                if np.isin(time, don_timestamp) == True and safezone_keep <= 0:
+                    tja += "1"
+                    safezone_keep = safezone
+                elif np.isin(time, ka_timestamp) == True and safezone_keep <= 0:
+                    tja += "2"
+                    safezone_keep = safezone
+                else:
+                    tja += "0"
+                    safezone_keep -= 1
+                if time % 100 == 99:
+                    tja += ",\n"
+            if length % 100 != 0:
+                tja += "0" * (100 - (length % 100)) + ",\n"
+            tja += "#END\n\n"
+        except:
+            pass
 
     return tja
